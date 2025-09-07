8737e8aa20c6954a6abe1e7bbc38e6059de77267
sangsoo <wsckm1@gmail.com>
2025-09-07 23:55:44 +0900

    style: prettier formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
-import { createRoot } from "react-dom/client";
+import App from './App.tsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
-    <App/>
-  </ChakraProvider>
-)
+    <App />
+  </ChakraProvider>,
+);
