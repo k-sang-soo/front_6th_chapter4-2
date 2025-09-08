56766f7f7fa3d6a9ebad1f1f0ef7be95edc24e08
sangsoo <wsckm1@gmail.com>
2025-09-08 15:45:30 +0900

    refactor: promise.all 병렬 실행될 수 있게 수정

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -88,13 +88,12 @@ const fetchLiberalArts = () => axios.get<Lecture[]>('/schedules-liberal-arts.jso
 // TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
 const fetchAllLectures = async () =>
   await Promise.all([
-    (console.log('API Call 1', performance.now()), await fetchMajors()),
-    (console.log('API Call 2', performance.now()), await fetchLiberalArts()),
-    (console.log('API Call 3', performance.now()), await fetchMajors()),
-    (console.log('API Call 4', performance.now()), await fetchLiberalArts()),
-    (console.log('API Call 5', performance.now()), await fetchMajors()),
-
-    (console.log('API Call 6', performance.now()), await fetchLiberalArts()),
+    (console.log('API Call 1', performance.now()), fetchMajors()),
+    (console.log('API Call 2', performance.now()), fetchLiberalArts()),
+    (console.log('API Call 3', performance.now()), fetchMajors()),
+    (console.log('API Call 4', performance.now()), fetchLiberalArts()),
+    (console.log('API Call 5', performance.now()), fetchMajors()),
+    (console.log('API Call 6', performance.now()), fetchLiberalArts()),
   ]);
 
 const LectureRow = memo(
